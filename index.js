ca6774778f1b29844e92befd0d02a2ce80307a81
Sai Charan Poosala <saicharan9071@gmail.com>
2024-06-15 12:49:59 +0530

    new

---
+++ index.js
@@ -0,0 +1,39 @@
+const express=require('express')
+const cors=require("cors")
+const mongoose =require("mongoose")
+
+const EmployeeModel=require('./Employes')
+const app =express()
+
+require('dotenv').config()
+app.use(cors())
+app.use(express.json())
+
+//mongoose.connect("mongodb://localhost:27017/NewEmployee")
+//mongoose.connect("mongodb+srv://saicharan:saicharan@cluster0.beqbnsq.mongodb.net/")
+mongoose.connect(process.env.MONGO_URL)
+
+
+app.post('/',(req,res)=>{
+     EmployeeModel.create(req.body)
+     
+     .then(employees01=>res.json(employees01))
+     .catch(err=>res.json(err))
+});
+
+app.post('/login',(req,res)=>{
+     const {email,password}=req.body;
+     EmployeeModel.findOne({email:email,password:password})
+     .then(user=>{
+          if(user){
+               if(user.password===password){
+                    res.json("Success")
+               }else{
+                    res.json("Password didn't match")
+               }
+          }else{
+               res.json("No record found")
+          }
+     })
+})
+app.listen(process.env.PORT,console.log("server running"))
